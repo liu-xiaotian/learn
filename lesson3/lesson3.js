062b6b838cce687558651c3f4d059e261e6414d8
tt <xiaotian3303@outlook.com>
2026-02-03 22:55:22 +0800

    JS引擎解析代码的底层逻辑

---
+++ lesson3/lesson3.js
@@ -0,0 +1,7 @@
+// "use strict"
+// x = 10
+
+// function f(a,a){}
+
+eval("var x = 2"); console.log(x);
+
---
+++ lesson3/main.js
@@ -0,0 +1,16 @@
+main();
+
+function main() {
+  // console.log("hello world");
+  
+  console.log(user);
+  var user = { name: "xiaoyao" };
+
+  console.log(age);
+  let age = 33;
+
+  sayHi();
+  function sayHi() {
+    console.log("hi");
+  }
+}
