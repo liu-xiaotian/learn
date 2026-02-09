d8d7f2ebbad352f56088bfd34b2f5d237243c963
tt <xiaotian3303@outlook.com>
2026-02-09 18:20:21 +0800

    day05

--- lesson04/lesson4.js
+++ lesson04/lesson4.js
@@ -17,7 +17,7 @@
 
 // console.log();
 
-var i = 0
+var i = 0;
 // for (var i=0; i<3; i++) setTimeout(()=>console.log(i))
 // for (let i=0; i<3; i++) setTimeout(()=>console.log(i))
 // for (var i=0; i<3; i++) console.log(i)
@@ -25,14 +25,80 @@ var i = 0
 //词法作用域 lexical scope
 
 // 课后题
-function lexicalFn(){
-  console.log('lexical2',lexical2);
-}
+// function lexicalFn1(){
+//   console.log('lexical2',lexical2);
+// }
 
-function lexicalFn2(){
-  let lexical2 = 'hello'
-  lexicalFn()
-}
-lexicalFn2()
+// function lexicalFn2(){
+//   let lexical2 = 'hello'
+//   lexicalFn1()
+// }
+// lexicalFn2()
 
 //词法作用域：词法作用域就是指作用域是由代码中函数声明的位置来决定的
+// function lexicalFn1(lexical2){
+//   console.log('lexical2',lexical2);
+// }
+
+// function lexicalFn2(){
+//   let lexical2 = 'hello'
+//   lexicalFn1(lexical2)
+// }
+// lexicalFn2()
+
+// function lexicalFn2() {
+//   let lexical2 = "hello";
+//   function lexicalFn1() {
+//     console.log("lexical2", lexical2);
+//   }
+//   // lexicalFn1();
+//   return lexicalFn1;
+// }
+// const fn3 = lexicalFn2();
+
+// fn3();
+
+// function lexicalFn2() {
+//   let lexical2 = "hello";
+//   function lexicalFn1() {
+//     console.log("lexical2", lexical2);
+//   }
+//   // lexicalFn1();
+//   return lexicalFn1;
+// }
+// lexicalFn2()();
+
+//1. 闭包会导致内存泄漏
+//2. 持久闭包、短暂闭包
+
+function make() {
+  let n = 0;
+  return () => ++n;
+}
+const c1 = make();
+const c2 = make();
+
+c1();
+c1();
+c1();
+c2(); //创建了一个新的闭包，
+// 1 2 3 1
+
+function closureFn() {
+  let num = 0;
+  // function addFn() {
+  //   num++;
+  // }
+  return {
+    get: () => num,
+    add: () => ++num,
+    // add: () => num + 1,
+  };
+}
+
+const result = closureFn();
+
+console.log(result.get());
+console.log(result.add());
+console.log(result.add());
+console.log(result.get());
---
+++ lesson05/exprssion.js
@@ -0,0 +1,9 @@
+async function fn() {
+  console.log("hello world");
+}
+console.log(fn());
+
+let x;
+console.log((x = "hello world"));
+console.log(0 && 5);
+console.log(0 || 5);
