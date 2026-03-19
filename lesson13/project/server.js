bd756ddab1328404b042b4b41ec76e85fc054f21
tt <xiaotian3303@outlook.com>
2026-03-19 13:18:37 +0800

    public/index.html

--- lesson13/project/server.js
+++ lesson13/project/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+// ===== 中间件 =====
+app.use(express.urlencoded({ extended: true })); // 解析 form
 
+// ===== 静态页面 =====
+// Express 会自动寻找 public/index.html。
+// 如果没有 index.html，你需要访问具体文件名，如 http://localhost:3000/login.html
+app.use(express.static("public"));
+
+// ===== 启动服务 =====
 app.listen(PORT, () => {
-  console.log("当前服务运行在 3000");
+  console.log(`Server running at http://localhost:${PORT}`);
 });
